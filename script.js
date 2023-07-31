4c47ada29e15f9a6072b764cbf27d5ab13b44cf1
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-07-31 14:01:54 +0200

    create basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let myLibrary = [];
+let myLibrary = [    { title: "Book 1", author: "Author 1", pages: 300, read: true },
+{ title: "Book 2", author: "Author 2", pagesC: 250, read: false }];
 
-function Book(title, author, pages, read) {
+function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -11,3 +12,47 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book){
     myLibrary.unshift(book);
 }
+
+    function displayBooks(booksArray) {
+        const container = document.getElementById('booksContainer');
+    
+        booksArray.forEach((book, index) => {
+            const bookDiv = document.createElement('div');
+            bookDiv.className = 'book';
+    
+            const title = document.createElement('div');
+            title.className = 'book-title';
+            title.textContent = book.title;
+    
+            const author = document.createElement('div');
+            author.className = 'book-author';
+            author.textContent = "Author: " + book.author;
+    
+            const pages = document.createElement('div');
+            pages.className = 'book-pages';
+            pages.textContent = "Pages: " + book.pages;
+    
+            const readButton = document.createElement('button');
+            readButton.className = `read-button ${book.read ? 'read' : 'unread'}`;
+            readButton.textContent = book.read ? "Read" : "Unread";
+            
+            readButton.addEventListener('click', function() {
+
+                myLibrary[index].read = !myLibrary[index].read;
+
+                readButton.classList.toggle('read');
+                readButton.classList.toggle('unread');
+                readButton.textContent = myLibrary[index].read ? "Read" : "Unread";
+            });
+    
+            bookDiv.appendChild(title);
+            bookDiv.appendChild(author);
+            bookDiv.appendChild(pages);
+            bookDiv.appendChild(readButton);
+            
+            container.appendChild(bookDiv);
+        });
+    }
+
+
+displayBooks(myLibrary);
